ed0fd0cf0090d8b9bf52ff3998a527edaec5d2e1
Mariselvam Ponnusamy <selvam245752@gmail.com>
2019-04-27 05:11:49 -0400

    Aritmetic operators

---
+++ Python-Operators.py
@@ -0,0 +1,25 @@
+# Python has 7 possible operators
+#1. Arithmetic
+  # +, -, *, /, **, %, //
+
+#Adding number
+#print(2+3)
+#Adding string(concat)
+#print('Mari'+'selvam')
+#Subtract number
+##Multiple number
+#print(2*3)
+#Division - Division will always return decimal number 2.0
+#print(10/5)
+#print(15/2)
+#print(15.0/2)
+#Exponent 5*5*5
+#print(5**3)
+#Modular - return remainder
+print(2%4)
+###Floor Division - retutn whole number
+#print(20//3)
+#operator precedence
+#print(4**2%2)#%, **
+print(2%4**2)
+print(1+2*3-4**2/3%2)
